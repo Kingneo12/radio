3d751bd983419e280f71ce4d5dee89d7d5421fc4
King Neo <m.ndwigar@gmail.com>
2023-12-03 09:01:12 +0300

    tailwind content update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [["./src/**/*.{js,jsx}"]],
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,jsx}"
+  ],
   mode: "jtit",
   theme: {
     extend: {
